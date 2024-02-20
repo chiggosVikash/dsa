2d17bf16ece9f8c6cac81d63d867ce7418c072eb
chiggosVikash <chiggos.vikash@gmail.com>
2024-02-20 22:20:16 +0530

    missing number from array

--- bin/dsa.dart
+++ bin/dsa.dart
@@ -1,19 +1,38 @@
-import 'package:dsa/ds/binary_tree.dart';
-import 'package:dsa/recursion/recursion.dart';
+import 'dart:collection';
 
 void main(List<String> arguments) {
-  final binaryTree = BinaryTree();
-
-  // final rec = Recursion();
-  // final total = rec.factorial(5);
-  // print("Fact value $total");
-
-  // final reversedStr = rec.revStr("Vikash");
-  // print("Reversed String $reversedStr");
-
-  // final totalSum = rec.addNumberToBoundaryNo(5);
-  // print("total sum $totalSum");
-
-  // final reversedNo = rec.revNumer(1908);
-  // print("reversed no $reversedNo");
+  final List<int> nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+  print(missingNumber(nums));
 }
+
+bool isPowerOfTwo(int n) {
+  if (n == 0) return false;
+
+  return (n & (n - 1)) == 0;
+}
+
+int removeDuplicates(List<int> nums) {
+  if (nums.isEmpty) return 0;
+
+  int i = 0;
+  for (int j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+}
+
+int missingNumber(List<int> nums) {
+  int n = nums.length;
+  int sum = (n * (n + 1)) ~/ 2;
+
+  for (int i = 0; i < n; i++) {
+    sum -= nums[i];
+  }
+  return sum;
+}
+
+
